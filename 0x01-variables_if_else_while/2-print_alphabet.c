bf40d54d140580e24f63d32b7989bb545ce99039
dawitalx <dawitvid@gmail.com>
2022-12-09 16:08:04 -0800

    code updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a = "a";
+while (a < z)
+{
+	putchar("%c",a);
+	a++;
+}
+putchar("%c",a);
+return (0);
+}
