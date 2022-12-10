e39d426df4805e82759c8d76d07de0c96185c427
dawitalx <dawitvid@gmail.com>
2022-12-09 16:50:37 -0800

    code updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a = 0;
+while (a <= 10)
+{
+        putchar(a);
+        a++;
+}
+putchar('\n');
+return (0);
