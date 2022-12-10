36c4ffa6f3567e154d3eceff305c8a60c256da5a
dawitalx <dawitvid@gmail.com>
2022-12-09 17:08:17 -0800

    code updated

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+	putchar("%d", &a);
+	a++;
+}
+putchar('\n');
+return (0);
+}
