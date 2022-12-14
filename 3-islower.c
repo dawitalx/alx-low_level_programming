79adead9c87a01f804b1aeee61652c8fc17c5134
dawitalx <dawitvid@gmail.com>
2022-12-14 10:10:27 -0800

    code updated

---
+++ 3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowe case character.
+ *
+ * Return: 1 for lowercase otherwise 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+    	{
+	       	return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
