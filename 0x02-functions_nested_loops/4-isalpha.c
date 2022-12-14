9baac39575f21fcd3789c30b1062ec7c4e0bca80
dawitalx <dawitvid@gmail.com>
2022-12-14 10:34:04 -0800

    code updated

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowe case character.
+ *@c: character to check`
+ * Return: 1 for lowercase otherwise 0.
+ */
+int _isalpha(int c)
+{
+        if (c >= 'a' && c <= 'z')
+        {
+		return (1);
+        }
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+        else
+        {
+		return (0);
+        }
+}
