16c7bc77a38bb3a86ded619ac6fdfe82eeaefba9
dawitalx <dawitvid@gmail.com>
2022-12-14 14:06:26 -0800

    code updated

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of anumber.
+ *@c: character to check`
+ * Return: 1
+ */
+int print_last_digit(int c)
+{
+	_putchar(c % 10);
+	return (0);
+}
