61e6141e1c5fc31ce59f867d3fb1d9e12152f646
dawitalx <dawitvid@gmail.com>
2022-12-14 06:13:07 -0800

    code updated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = '0';
+int b = '0'
+while (a <= '99')
+{
+	putchar(a,b);
+}
+putchar('\n');
+return (0);
+}
