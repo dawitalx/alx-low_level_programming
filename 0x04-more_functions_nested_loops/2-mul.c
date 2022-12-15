42f6950565ef0cb944bfa095a7ce32b6904853e7
dawitalx <dawitvid@gmail.com>
2022-12-15 04:48:24 -0800

    code updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two intigers
+ * @a: intiger to be checked
+ * @b: intiger to be checkd
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
