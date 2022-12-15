5341c7cd53e82c174b1325d66e6a07ba522c6744
dawitalx <dawitvid@gmail.com>
2022-12-15 09:25:54 -0800

    code update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - draw strait line in the terminal.
+ * @n: intiger to be checked
+ */
+
+void print_line(int n)
+{
+	while (n--)
+	{
+		_putchar('_');
+	}
+}
