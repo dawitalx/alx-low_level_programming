29300e9b00be5534077b64a1d7141e91578dd9dc
dawitalx <dawitvid@gmail.com>
2022-12-15 05:34:00 -0800

    code updated

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	while (i >= '0' && i <= '9')
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+		}
+		else
+		{
+			_putchar(i);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
