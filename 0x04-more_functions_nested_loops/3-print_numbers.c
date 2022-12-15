216056418eb819c69138b3c5081fb9df3c9b462a
dawitalx <dawitvid@gmail.com>
2022-12-15 05:09:55 -0800

    code updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,12 @@
 /**
  * print_numbers - print 0 to 9 followd by new line.
  *
- * Return: Void
+ * 
  */
 void print_numbers(void)
 {
-	int a = 0;
-	int check = 10;
-	while(check--)
+	for(int i = 0; i <= 9; i++)
 	{
-		_putchar(a);
-		_putchar('\n');
-		a++;
+		_putchar(i);
 	}
 }
