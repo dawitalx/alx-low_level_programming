9db2ce7c19cd469889f614f5546636368c4245f9
dawitalx <dawitvid@gmail.com>
2022-12-19 13:55:40 -0800

    code updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take apointer and update avalue of point.
+ * @n: intiger pointer to be cheked.
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
