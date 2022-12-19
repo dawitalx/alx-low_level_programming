c67b9cf75face1e86bdfc15fc03e9d144d7e84b6
dawitalx <dawitvid@gmail.com>
2022-12-19 15:53:31 -0800

    code updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,25 @@
  * @str: string to be printed.
  */
 
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str +=2;
-	}
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
