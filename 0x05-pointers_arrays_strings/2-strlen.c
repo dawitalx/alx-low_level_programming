1286418bf952fcf9522fd4027a2e7af6371d5173
dawitalx <dawitvid@gmail.com>
2022-12-19 14:16:22 -0800

    code updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of string.
+ * @s: characted to be checkd.
+ * Return: the number of string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
