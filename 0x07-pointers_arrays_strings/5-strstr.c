8841e739824297c68d7dc1a9eca92723cb5d6e66
dawitalx <dawitvid@gmail.com>
2023-01-03 14:05:49 -0800

    code updated

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strst - find the first occurance of substring
+ * @haystack: string to be searched
+ * @needle: string to be checked
+ * Return: pointer of needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+
+	return (NULL);
+}
