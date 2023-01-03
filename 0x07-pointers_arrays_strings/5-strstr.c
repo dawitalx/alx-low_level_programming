2711de05667e90ea2951db52fb67723d43581b99
dawitalx <dawitvid@gmail.com>
2023-01-03 14:10:52 -0800

    code updated

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,10 +10,10 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	int i = 0;
 	int j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
