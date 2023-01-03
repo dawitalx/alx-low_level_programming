dde62f303cdf6f36aee58abda08a1d5f55a0e7f6
dawitalx <dawitvid@gmail.com>
2023-01-03 14:38:33 -0800

    code updated

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; a[j][i] != '\0'; i++)
+		{
+			_putchar(a[j][i]);
+		}
+		_putchar('\n');
+	}
+}
