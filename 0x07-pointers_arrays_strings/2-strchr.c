2dbf443eaf6a85dea8c6f3595211b89aae4cb178
dawitalx <dawitvid@gmail.com>
2023-01-03 04:29:54 -0800

    code updated

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locate acharacter in a string
+ * @s: string to be manipulated
+ * @c: character to be locaated
+ * Return: pointer of c or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while(*s != '\n')
+	{
+		if(*s == c)
+		{
+			return(s);
+		}
+		s++;
+	}
+	return(NULL);
+}
