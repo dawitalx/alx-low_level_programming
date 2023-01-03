0b067cba0429c3434aff8bb91083da6085d1c696
dawitalx <dawitvid@gmail.com>
2023-01-03 13:09:26 -0800

    code updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int count = 0;
-	
-	while (*s != '\n')
+	int i = 0, j;
+	int matches = 0;
+
+	while (s[i] != '\0') /*iterate through string*/
 	{
-		while (*accept != '\n')
+
+		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
 		{
-			if (*s == *accept)
+			if (s[i] == accept[j]) /*record & break at first match*/
 			{
-				count++;
+				matches++;
+				break;
 			}
-			accept++;
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (matches);/*return if idx doesn't match*/
 		}
-		s++;
+		i++;
 	}
-	return (count);
+	return (matches); /* return num if all match till end */
+
 }
