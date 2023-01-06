96b30668980bddf2449b23f9c14194eedfb3895f
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-01-07 00:59:40 +0300

    Create 5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,24 @@
+char *_strstr(char *haystack, char *needle)
+{
+	/**
+	 * we initialize a helping variables
+	 * to assist in returning one of
+	 * our parameters pointers haystack
+	*/
+	char *h, *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack++;
+	}
+	return ('\0');
+}
