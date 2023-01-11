5471a37830408981845d13345c23439e4b44c03d
dawitalx <dawitvid@gmail.com>
2023-01-11 09:58:56 -0800

    code updated

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for(i = 0; i < argc; i++)
+			printf("%s\n",argv[i]);
+	}
+
+	return (0);
+}
