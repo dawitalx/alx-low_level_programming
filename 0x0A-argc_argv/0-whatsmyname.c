2712909012afc37e6dde4007ea5200dd57593b3c
dawitalx <dawitvid@gmail.com>
2023-01-11 08:29:46 -0800

    code updated

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints programs name
+ * @argc: number of commands
+ * @argv: array of command
+ */
+
+void main(int argc, char *argv)
+{
+	printf("%d", argv[0]);
+}
