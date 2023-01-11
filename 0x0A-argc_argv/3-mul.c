b2828cfc63d32a5a8b964441ccc5dcf67b5e5715
dawitalx <dawitvid@gmail.com>
2023-01-11 10:22:15 -0800

    code updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * main - multiple two number
  * argc: number of argument
  * argv: pointer of argument
- * Return: 0 or 1
+ * Return: 0 on success, 1 on error
  */
 
 int main(int argc, char *argv[])
@@ -18,6 +18,5 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 	return (0);
 }
