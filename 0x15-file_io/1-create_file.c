0d817ccd08764eb721420f9ec1b2ffdc723b56ea
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-02-07 01:04:53 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,13 @@ int _strlen(char *str)
 	return (len);
 }
 
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
