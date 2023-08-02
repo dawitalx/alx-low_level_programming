cb2f0e06f27cdd1775278148afd2d3b7ccec5bb3
dawitalx <dawitvid@gmail.com>
2023-08-02 07:20:27 -0700

    code updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,19 @@
 #include "main.h"
 
 /**
- * void _puts_recursion(char *s) - prints character given
+ * _puts_recursion - prints character given
  * @s: character to be printed
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 	{
 		_putchar('\n');
