24ef965ef95e2da29a82352edf071a1b422b9b48
dawitalx <dawitvid@gmail.com>
2023-08-02 05:38:37 -0700

    code updated

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * void _puts_recursion(char *s) - prints character given
+ * @s: character to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
