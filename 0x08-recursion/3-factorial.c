3befaf99c606bbb35e452862d7e6bc65995f2a64
dawitalx <dawitvid@gmail.com>
2023-08-02 08:45:11 -0700

    code updated

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -13,10 +13,16 @@ int factorial(int n)
 
 	if (n == 0)
 	{
-		return 1;
+		return (1);
+	}
+	else if (N > 0)
+	{
+		fact = n * factorial(n - 1);
+	}
+	else
+	{
+		fact = n * factorial(n + 1);
 	}
 
-	fact = n * factorial(n - 1);
-
-	return fact;
+	return (fact);
 }
