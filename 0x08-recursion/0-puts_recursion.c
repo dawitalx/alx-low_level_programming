9451b1dfae49ac5de79fc2c59d9aab65d9bfb9a0
dawitalx <dawitvid@gmail.com>
2023-08-02 06:04:08 -0700

    code updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,24 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * void _puts_recursion(char *s) - prints character given
  * @s: character to be printed
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 {
+	if(*s)
+	{
 	while (*s != EOF)
 	{
 		_putchar(*s);
 		s++;
+	}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,10 @@
-#ifndef Main_H
-#define Main_H
-
-/*
- * _puts_recursion - prints character givien
- * @s: character to be printed
- */
-
+int _putchar(char c);
 void _puts_recursion(char *s);
-
-#endif
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
