db168ac6e60331a34d3e0e0143e98d73a2752eb4
dawitalx <dawitvid@gmail.com>
2023-08-02 09:07:07 -0700

    code updated

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Return x the power of y
+ * @x: the base intiger
+ * @y: y number of time x to be multiplied
+ * Return: the power of x of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = 1;
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	result = x * _pow_recursion(x, (y - 1));
+
+	return (result);
+}
