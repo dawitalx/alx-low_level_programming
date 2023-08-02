0fc9ec7d66c5a1348c60f2d23d2ec4bb97e32835
dawitalx <dawitvid@gmail.com>
2023-08-02 08:39:00 -0700

    code updated

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial
+ * @n: intiger to be factorialized
+ * Return: the factorial of n
+ */
+
+int factorial(int n)
+{
+	int fact = 1;
+
+	if (n <= 1)
+	{
+		return 1;
+	}
+
+	fact = n * factorial(n - 1);
+
+	return fact;
+}
