6a77a93260a2d7a879db7fee5248d235a388e8e1
dawitalx <dawitvid@gmail.com>
2023-08-05 06:01:26 -0700

    code updated

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+  
+/**
+ * _strncat - function
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: just 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        return 0;
+}
