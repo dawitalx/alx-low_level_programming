74681f2108c2b3436c005825de09c9456cd3c136
dawitalx <dawitvid@gmail.com>
2023-08-06 02:21:19 -0700

    code updated

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - function prints its  program name
+ * @argc: int
+ * @argv: list of arguments
+ * Return: just 0
+ */
+
+int main(argc, *argv[])
+{
+	_putchar("%d\n", argv[0]);
+}
