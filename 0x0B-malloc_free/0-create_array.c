b8beef77653ce3ab6927925f13114555299b9728
dawitalx <dawitvid@gmail.com>
2023-08-07 14:06:27 -0700

    cod updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,19 @@
 char *create_array(unsigned int size, char c)
 {
 	int len = size;
-	int cnum = c;
+	char *array;
+	unsigned int i;
 	if (len == 0)
 	{
 		return (NULL);
 	}
 	else if (size > 0)
 	{
-		return (malloc(sizeof(cnum) * size));
+		array = (malloc(sizeof(char) * size));
+		for (i = 0; i < size; i++)
+		array[i] = c;
+
+		return (array);
 	}
 	else
 	{
