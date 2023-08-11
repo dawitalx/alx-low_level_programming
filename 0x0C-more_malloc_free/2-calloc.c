ca0eb0b056b362403d420492a5b64201999661ed
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-11 22:04:24 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,19 +9,21 @@
  * Return: pointer to the memory area s
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size) {
-    if (nmemb == 0 || size == 0) {
-        return NULL; // Return NULL if either nmemb or size is 0
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    if (nmemb == 0 || size == 0)
+    {
+        return NULL;
     }
-
-    void *ptr = malloc(nmemb * size); // Allocate memory
-
-    if (ptr != NULL) {
-        // Use memset to set the allocated memory to zero
+    
+    void *ptr = malloc(nmemb * size);
+    
+    if (ptr != NULL)
+    {
         memset(ptr, 0, nmemb * size);
     }
 
-    return ptr;
+    return (ptr);
 }
 
 int main(void) {
