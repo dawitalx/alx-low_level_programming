fc73edda1fc459ac65c611148e32ebfe36053cdb
dawitalx <dawitvid@gmail.com>
2023-08-25 17:16:43 -0700

    code updated

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - add new node at the beginning 
+ * @head: orginal node
+ * @srr: str to beadded
+ * Return: address or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	typedef struct list_t
+	{
+		char *str;
+		struct list_t *next;
+	} list_t;
+	
+	if (str == NULL)
+		return (NULL);
+
+	list_t *newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+	
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = *head;
+	*head = newNode;
+	
+	return (newNode);
+}
