792e4e17c6ffd4c567343755affffb478fab7653
dawitalx <dawitvid@gmail.com>
2023-08-30 06:52:40 -0700

    code updated

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print reverse
+ * @s: string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar("\n");
+	}
+	
+	s++;
+	_print_rev_recursion(*s);
+	_putchar(*s);
+}
