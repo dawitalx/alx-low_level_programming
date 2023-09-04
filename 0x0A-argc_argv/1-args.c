6a7278bc41b41daadc6f3db28da7342887dfd805
dawitalx <dawitvid@gmail.com>
2023-09-04 13:31:21 -0700

    code updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - return no of arg
+ * @argc: no of arg
+ * @argv: list of arg
+ * @Return: 0
+ *
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+	printf("%d\n", agc - 1);
+
+	return (0);
+}
