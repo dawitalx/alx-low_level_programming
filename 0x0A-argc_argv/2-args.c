92b221d74ad5bea60e58ff758d87dae56a2918c2
dawitalx <dawitvid@gmail.com>
2023-09-06 04:52:49 -0700

    code updated

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: number of argument
+ * @argv: pointer of argument
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
