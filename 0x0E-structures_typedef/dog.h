fe0594509525593af541d88db43cc3890e85fa14
dawitalx <dawitvid@gmail.com>
2023-09-12 13:53:09 -0700

    code updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,9 @@
-struct dog {
-    char name[50];
-    float age;
-    char owner[50];
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
